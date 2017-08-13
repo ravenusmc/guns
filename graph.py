610d2b127bb7f3e45019e0aed1bc2404cdd72ac3
ravenusmc <mcuddy77@gmail.com>
2017-08-13 17:50:58 -0400

    changed graph to bar

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 #This file will handle all of the data for the graphs on the see.html page. 
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import CategoricalColorMapper, HoverTool
+from bokeh.models import ColumnDataSource, Range1d, LabelSet, Label
 import numpy as np
 import pandas as pd 
 
@@ -14,11 +15,13 @@ class Graphs():
     def by_year(self):
         #This is where 
         output_file('by_year.html')
-        #Setting up the dimensions of the graph.
-        p = figure(title="Deaths by Year", plot_width=400, plot_height=400)
         #setting up a list for the specific years and number of deaths. 
         years = [2012,2013,2014]
+        years = pd.to_datetime(years)
         number_of_deaths = []
+        #Setting up the dimensions of the graph and its labels.
+        plot = figure(title="Deaths by Year", x_axis_label='Years', y_axis_label='Deaths', plot_width=400, 
+            plot_height=400, x_axis_type="datetime")
         #This count variable will hold the number of loops as well as position in the list
         count = 0
         while count < 3:
@@ -28,17 +31,19 @@ class Graphs():
             number_of_deaths.append(deaths_by_year)
             #increasing the count by one. 
             count += 1
-        #rendering the graph 
-        p.line(years, number_of_deaths, legend="Deaths", line_color='blue', line_width=2)
-        p.xaxis.axis_label = "Year"
-        p.yaxis.axis_label = "Deaths"
-        hover = p.select_one(HoverTool)
-        hover.tooltips = [('Year', '@years'),
-            ('Deaths', '@number_of_deaths')]
-        show(p)
+        #rendering the graph
+        plot.vbar(x=years, width=0.3, bottom=33500, top=number_of_deaths, legend="Deaths", line_color='blue', line_width=2) 
+        # plot.line(years, number_of_deaths, legend="Deaths", line_color='blue', line_width=2)
+        plot.legend.location = (0,300)
+        show(plot)
 
 test = Graphs()
 test.by_year()
+
+
+# hover = p.select_one(HoverTool)
+# hover.tooltips = [('Year', '@years'),
+#     ('Deaths', '@number_of_deaths')]
 
 
 
